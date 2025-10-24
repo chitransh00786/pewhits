8f7526ef9c98158afa8bb53d6de7288ace3acd11
chitransh00786 <138363557+chitransh00786@users.noreply.github.com>
2025-10-24 15:39:02 +0530

    Initial commit

---
+++ Old/config/config.py
@@ -0,0 +1,11 @@
+class Authorization:
+    ngrok_auth_token = "2ryTNIRJt7EfI8YH911S9pw3SGN_2bXVyi1ZdVMHAWgF6vJ6d"
+    ngrok_domain = "stirring-thoroughly-kid.ngrok-free.app"
+    SPOTIPY_CLIENT_ID = '61f786cf566f4e2489c4f0507a3e059e'
+    SPOTIPY_CLIENT_SECRET = '02786c3a37cf4e6f8fe56f5d5a73aae3'
+    
+class DJ:
+    playlists = [
+        "https://open.spotify.com/playlist/5cSazwVC79ajs382BdSZC0?si=Oai7IHDMS8u7MDzVTyo-wg",
+        "https://open.spotify.com/playlist/1BfpZk1ImUkjksKM0wUa4U?si=J_N-YfhHRE2nZ8tmv_nBLg"
+    ]
